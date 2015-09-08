184809ae7c1fb23f5b865d325087c9f4ae3b5c18
Mike Dunn <mdunn@thedailydot.com>
2015-09-08 11:54:19 -0500

    updating

--- classlist.js
+++ classlist.js
@@ -1,5 +1,8 @@
 (function(){
 
+  // used with permission of mike dunn
+  // https://github.com/moagrius/classList
+
   /**
    * uses work by
    * https://github.com/remy/polyfills/blob/master/classList.js
@@ -24,92 +27,107 @@
       return i;
     };
 
-    var DOMTokenList = function(element){
-      this.element = element;
-    };
-    DOMTokenList.prototype.contains = function(name){
-      var classes = this.element.className.split(/\s+/);
-      return indexOf(classes, name) != -1;
-    };
-    DOMTokenList.prototype.add = function(){
-      var classes = this.element.className.split(/\s+/);
-      for(var i = arguments.length - 1; i >= 0; i--) {
-        var name = arguments[i];
-        if(indexOf(classes, name) == -1){
-          classes.push(name);
-        }
-      }
-      this.element.className = classes.join(' ');
-    };
-    DOMTokenList.prototype.remove = function(name){
-      var classes = this.element.className.split(/\s+/);
-      for(var i = arguments.length - 1; i >= 0; i--) {
-        var index = indexOf(classes, name);
-        if(index != -1){
-          classes.splice(index, 1);
-        }
-      }
-      this.element.className = classes.join(' ');
-    };
-    DOMTokenList.prototype.toggle = function(name, force){
-      var exists = this.contains(name);
-      var added = false;
-      if(exists){
-        if(!force){
-          this.remove(name);
-        }
-      } else {
-        if(force !== false){
-          this.add(name);
-          added = true;
-        }
-      }
-      return (!!force === force) ? force : added;
-    };
+    // scope it so it's not hoisted, otherwise IE10 will fail to patch
+    (function(){
 
-    if(Object.defineProperty) {
-      Object.defineProperty(Element.prototype, 'classList',{
-        get : function(){
-          return new DOMTokenList(this);
+      var DOMTokenList = function(element){
+        this.element = element;
+      };
+      DOMTokenList.prototype.contains = function(name){
+        var classes = this.element.className.split(/\s+/);
+        return indexOf(classes, name) != -1;
+      };
+      DOMTokenList.prototype.add = function(){
+        var classes = this.element.className.split(/\s+/);
+        for(var i = arguments.length - 1; i >= 0; i--) {
+          var name = arguments[i];
+          if(indexOf(classes, name) == -1){
+            classes.push(name);
+          }
         }
-      });
-    } else if(Element.prototype.__defineGetter__){
-      Element.prototype.__defineGetter__('classList', function(){
-        return new DOMTokenList(this);
-      });
-    }
+        this.element.className = classes.join(' ');
+      };
+      DOMTokenList.prototype.remove = function(name){
+        var classes = this.element.className.split(/\s+/);
+        for(var i = arguments.length - 1; i >= 0; i--) {
+          var index = indexOf(classes, name);
+          if(index != -1){
+            classes.splice(index, 1);
+          }
+        }
+        this.element.className = classes.join(' ');
+      };
+      DOMTokenList.prototype.item = function(index){
+        var classes = this.element.className.split(/\s+/);
+        return classes[index];
+      };
+      DOMTokenList.prototype.toggle = function(name, force){
+        var exists = this.contains(name);
+        if(exists === force){
+          return force;
+        }
+        if(exists){
+          this.remove(name);
+        } else {
+          this.add(name);
+        }
+        return !exists;
+      };
+      // replaced with getter, not supported in IE8, will always return 0
+      DOMTokenList.prototype.length = 0;
+
+      if(Object.defineProperty) {
+        Object.defineProperty(Element.prototype, 'classList',{
+          get : function(){
+            return new DOMTokenList(this);
+          }
+        });
+        Object.defineProperty(DOMTokenList.prototype, 'length', function(){
+          var classes = this.element.className.split(/\s+/);
+          return classes.length;
+        });
+      } else if(Element.prototype.__defineGetter__){
+        Element.prototype.__defineGetter__('classList', function(){
+          return new DOMTokenList(this);
+        });
+      }
+
+    })();
 
   } else {  // we have support, just patch methods as needed
 
-    // test and patch multiple argument support
-    tester.classList.add('a', 'b');
-    if(!tester.classList.contains('b')){
-      var methods = ['add', 'remove'];
-      var patch = function(definition, method){
-        var historic = definition[method];
-        definition[method] = function(){
-          for(var i = arguments.length - 1; i >= 0; i--){
-            var token = arguments[i];
-            historic.call(this, token);
-          }
-        };
-      };
-      for(var i = methods.length - 1; i >= 0; i--){
-        var method = methods[i];
-        patch(DOMTokenList.prototype, method);
-      }
-    }
+    if('DOMTokenList' in this){  // this should be true if classList is detected
 
-    // test and patch toggle with force
-    tester.classList.toggle('c', false);
-    if(tester.classList.contains('c')){
-      var historic = DOMTokenList.prototype.toggle;
-      DOMTokenList.prototype.toggle = function(token, force){
-        if (arguments.length > 0 && this.contains(token) === force) {
-          return force;
+      // test and patch multiple argument support
+      tester.classList.add('a', 'b');
+      if(!tester.classList.contains('b')){
+        var methods = ['add', 'remove'];
+        var patch = function(definition, method){
+          var historic = definition[method];
+          definition[method] = function(){
+            for(var i = arguments.length - 1; i >= 0; i--){
+              var token = arguments[i];
+              historic.call(this, token);
+            }
+          };
+        };
+        for(var i = methods.length - 1; i >= 0; i--){
+          var method = methods[i];
+          patch(DOMTokenList.prototype, method);
         }
-        return historic.call(this, token);
-      };
+      }
+
+      // test and patch toggle with force
+      tester.classList.toggle('c', false);
+      if(tester.classList.contains('c')){
+        var historic = DOMTokenList.prototype.toggle;
+        DOMTokenList.prototype.toggle = function(token, force){
+          if (arguments.length > 0 && this.contains(token) === force) {
+            return force;
+          }
+          return historic.call(this, token);
+        };
+      }
 
     }
 
